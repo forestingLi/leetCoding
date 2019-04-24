77c2b3dfa1ef48737d0c430d3eeaf5f5ce33c96f
lic34 <chenglin.li@emc.com>
2019-04-24 14:54:49 +0800

    commit for main()

---
+++ leetcode/main.py
@@ -0,0 +1,21 @@
+from leetcode.solutions import *
+
+if __name__ == '__main__':
+    # test_string = "aba"
+    # test_list = ['a','b','c']
+    # test_string = ''.join(test_list)
+    #
+    # print(test_string)
+    # string_lenth = len(test_string)
+    # print(test_string)
+    # char_list = list(test_string)
+    # for i in range(3,0,-1):
+    #     i = i + 1
+    #     print(i)
+    #     print("\n")
+    # print(char_list[0:string_lenth])
+    str = Solution.longestPalindrome(Solution,"")
+    if str :
+        print (str)
+    else:
+        print("string is None")
