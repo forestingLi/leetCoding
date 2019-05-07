eb28d6f51db92ed714ff97ad24d18a8baec161af
lic34 <chenglin.li@emc.com>
2019-05-07 16:52:52 +0800

    remaing twoSum/solutions.py to  two_sum.py

---
+++ leetcode/two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    '''https://leetcode.com/problems/two-sum/'''
+    def twoSum(self, nums: list(), target: int) -> list():
+        for index,num in enumerate(nums):
+            next_num = target - num
+            if next_num in nums[index+1:]:
+                next_index = nums.index(next_num,index+1,)
+                return [index,next_index]
+            else:
+                continue
+
+
+
